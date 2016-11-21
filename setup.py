f0ccbc23fd7a23af6101489688de23f80a93b17c
Jim Abramson <jim@datadoghq.com>
2016-11-21 12:43:20 -0500

    try a different version name

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
     name='logmatic-python',
-    version='0.1.6a',
+    version='0.1.6a0',
     author='Logmatic.io support team',
     author_email='support@logmatic.io',
     packages = ['logmatic'],
     scripts=[],
     url='https://github.com/jimabramson/logmatic-python',
-    download_url = 'https://github.com/jimabramson/logmatic-python/tarball/0.1.6a',
+    download_url =
+    'https://github.com/jimabramson/logmatic-python/tarball/0.1.6a0',
     license='MIT',
     long_description=open('README.rst').read(),
     description='Python plugin to send logs to Logmatic.io',
